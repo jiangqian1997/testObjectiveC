d5cbcef5a8919e031e644f629faa3b16c4bea510
jiangqian1997 <jiangqian1997@gmail.com>
2017-04-30 16:24:30 +0800

    first commit

---
+++ testObjectiveC/testObjectiveC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testObjectiveC
+//
+//  Created by JiangQian on 30/04/2017.
+//  Copyright © 2017 JiangQian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ testObjectiveC/testObjectiveC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testObjectiveC
+//
+//  Created by JiangQian on 30/04/2017.
+//  Copyright © 2017 JiangQian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
